cb9d992bc9ddecde3d9ac9435d0797bcb0977632
Andrei <andreiilinescu02@gmail.com>
2024-09-24 23:44:56 +0200

    added control equivalent gates

--- QuantumSimulator.py
+++ QuantumSimulator.py
@@ -6,119 +6,130 @@ import numpy as np
 import matplotlib.pyplot as plt
 from termcolor import colored
 
-class QuantumSimulator:
-    def __init__(self,qbits:int):
-        self.qbits=qbits
-        self.state=npt.init_state(qbits)
-        self.qiskit_circ=QuantumCircuit(qbits)
 
-    def h(self,qbit:int):
-        self.state=npt.apply_one_qubit_gate(self.state,npt.H(),qbit)
+class QuantumSimulator:
+    def __init__(self, qbits: int):
+        self.qbits = qbits
+        self.state = npt.init_state(qbits)
+        self.qiskit_circ = QuantumCircuit(qbits)
+
+    def h(self, qbit: int):
+        self.state = npt.apply_one_qubit_gate(self.state, npt.H(), qbit)
         self.qiskit_circ.h(qbit)
 
-    def x(self,qbit:int):
-        self.state=npt.apply_one_qubit_gate(self.state,npt.X(),qbit)
+    def x(self, qbit: int):
+        self.state = npt.apply_one_qubit_gate(self.state, npt.X(), qbit)
         self.qiskit_circ.x(qbit)
-    
-    def y(self,qbit:int):
-        self.state=npt.apply_one_qubit_gate(self.state,npt.Y(),qbit)
+
+    def y(self, qbit: int):
+        self.state = npt.apply_one_qubit_gate(self.state, npt.Y(), qbit)
         self.qiskit_circ.y(qbit)
-    
-    def z(self,qbit:int):
-        self.state=npt.apply_one_qubit_gate(self.state,npt.Z(),qbit)
+
+    def z(self, qbit: int):
+        self.state = npt.apply_one_qubit_gate(self.state, npt.Z(), qbit)
         self.qiskit_circ.z(qbit)
-    
-    def s(self,qbit:int):
-        self.state=npt.apply_one_qubit_gate(self.state,npt.S(),qbit)
+
+    def s(self, qbit: int):
+        self.state = npt.apply_one_qubit_gate(self.state, npt.S(), qbit)
         self.qiskit_circ.s(qbit)
 
-    def sdg(self,qbit:int):
-        self.state=npt.apply_one_qubit_gate(self.state,npt.SDG(),qbit)
+    def sdg(self, qbit: int):
+        self.state = npt.apply_one_qubit_gate(self.state, npt.SDG(), qbit)
         self.qiskit_circ.sdg(qbit)
-    
-    def t(self, qbit:int):
-        self.state=npt.apply_one_qubit_gate(self.state,npt.T(),qbit)
+
+    def t(self, qbit: int):
+        self.state = npt.apply_one_qubit_gate(self.state, npt.T(), qbit)
         self.qiskit_circ.t(qbit)
-    
-    def p(self,qbit:int,angle:float):
+
+    def p(self, qbit: int, angle: float):
         """
-            Args:
-                qbit (int): The number of the qbit we are applying the gate to
-                angle (float): The phase shift angle in radians
+        Args:
+            qbit (int): The number of the qbit we are applying the gate to
+            angle (float): The phase shift angle in radians
         """
-        self.state=npt.apply_one_qubit_gate(self.state,npt.P(angle),qbit)
-        self.qiskit_circ.p(angle,qbit)
+        self.state = npt.apply_one_qubit_gate(self.state, npt.P(angle), qbit)
+        self.qiskit_circ.p(angle, qbit)
 
-    def cnot(self,control:int,target:int):
-        self.state=npt.apply_two_qubit_gate(self.state,npt.CNOT(),control,target)
-        self.qiskit_circ.cx(control,target)
-    
-    def ch(self,control:int,target:int):
-        self.state=npt.apply_two_qubit_gate(self.state,npt.CH(),control,target)
-        self.qiskit_circ.ch(control,target)
+    def cnot(self, control: int, target: int):
+        self.state = npt.apply_two_qubit_gate(self.state, npt.CNOT(), control, target)
+        self.qiskit_circ.cx(control, target)
 
-    def cx(self,control:int,target:int):
-        self.state=npt.apply_two_qubit_gate(self.state,npt.CX(),control,target)
-        self.qiskit_circ.cx(control,target)
-    
-    def cy(self,control:int,target:int):
-        self.state=npt.apply_two_qubit_gate(self.state,npt.CY(),control,target)
-        self.qiskit_circ.cy(control,target)
-    
-    def cz(self,control:int,target:int):
-        self.state=npt.apply_two_qubit_gate(self.state,npt.CZ(),control,target)
-        self.qiskit_circ.cz(control,target)
-    
-    def cs(self,control:int,target:int):
-        self.state=npt.apply_two_qubit_gate(self.state,npt.CS(),control,target)
-        self.qiskit_circ.cs(control,target)
+    def ch(self, control: int, target: int):
+        self.state = npt.apply_two_qubit_gate(self.state, npt.CH(), control, target)
+        self.qiskit_circ.ch(control, target)
 
+    def cx(self, control: int, target: int):
+        self.state = npt.apply_two_qubit_gate(self.state, npt.CX(), control, target)
+        self.qiskit_circ.cx(control, target)
 
+    def cy(self, control: int, target: int):
+        self.state = npt.apply_two_qubit_gate(self.state, npt.CY(), control, target)
+        self.qiskit_circ.cy(control, target)
+
+    def cz(self, control: int, target: int):
+        self.state = npt.apply_two_qubit_gate(self.state, npt.CZ(), control, target)
+        self.qiskit_circ.cz(control, target)
+
+    def cs(self, control: int, target: int):
+        self.state = npt.apply_two_qubit_gate(self.state, npt.CS(), control, target)
+        self.qiskit_circ.cs(control, target)
+
+    def ct(self, control: int, target: int):
+        self.state = npt.apply_two_qubit_gate(self.state, npt.CT(), control, target)
+        self.qiskit_circ.ct(control, target)
+
+    def cp(self, control: int, target: int, angle: float):
+        self.state = npt.apply_two_qubit_gate(self.state, npt.CP(angle), control, target)
+        self.qiskit_circ.cp(angle, control, target)
 
     def get_qiskit_statevector(self) -> Statevector:
-        circ=self.qiskit_circ.copy()
+        circ = self.qiskit_circ.copy()
         circ.save_statevector()
         # Transpile for simulator
-        simulator = AerSimulator(method='statevector')
+        simulator = AerSimulator(method="statevector")
         circ = transpile(circ, simulator)
         # Run and get statevector
         result = simulator.run(circ).result()
         statevector = result.get_statevector(circ)
         return statevector
-    
+
     def measure_qiskit_probablities(self) -> np.array:
-        statevector=self.get_qiskit_statevector()
-        probs=statevector.probabilities(decimals=5)
+        statevector = self.get_qiskit_statevector()
+        probs = statevector.probabilities(decimals=5)
         return np.array(probs)
-    
+
     def measure_own_probabilities(self) -> np.array:
-        out=np.array(self.state)
-        out=np.around(np.abs(out.flatten()).__pow__(2),5)
+        out = np.array(self.state)
+        out = np.around(np.abs(out.flatten()).__pow__(2), 5)
         return out
-    
+
     def plot_qiskit_probabilities(self):
-        probs=self.measure_qiskit_probablities()
+        probs = self.measure_qiskit_probablities()
         n = self.qbits
-        labels = [f'{i:0{n}b}' for i in range(2**n)]
+        labels = [f"{i:0{n}b}" for i in range(2**n)]
         print(probs)
-        plt.bar(labels,probs, color='skyblue')
-        plt.xlabel('Quantum States')
-        plt.ylabel('Probability')
-        plt.title('Probability Distribution of Qiskit Quantum States')
+        plt.bar(labels, probs, color="skyblue")
+        plt.xlabel("Quantum States")
+        plt.ylabel("Probability")
+        plt.title("Probability Distribution of Qiskit Quantum States")
         plt.ylim(0, 1)
         plt.show()
-    
+
     def plot_own_probabilities(self):
         npt.plot_nqbit_prob(self.state)
 
     def draw_qiskit_circuit(self):
         print(self.qiskit_circ.draw())
-    
+
     def compare_results(self):
-        own=self.measure_own_probabilities()
-        qiskit=self.measure_qiskit_probablities()
-        result=colored("same","green") if np.array_equal(own,qiskit) else colored("different","red")
-        
+        own = self.measure_own_probabilities()
+        qiskit = self.measure_qiskit_probablities()
+        result = (
+            colored("same", "green")
+            if np.array_equal(own, qiskit)
+            else colored("different", "red")
+        )
+
         print("Own:")
         print(own)
         print("Qiskit:")
@@ -126,7 +137,7 @@ class QuantumSimulator:
         print(f"Conclusion: {result}")
 
     @classmethod
-    def setup_bell_state(cls, num_qubits: int) -> 'QuantumSimulator':
+    def setup_bell_state(cls, num_qubits: int) -> "QuantumSimulator":
         """
         Sets up a Bell state with the specified number of qubits.
 
@@ -137,17 +148,21 @@ class QuantumSimulator:
             QuantumSimulator: An instance of QuantumSimulator with GHZ state prepared.
         """
         if num_qubits < 2:
-            raise ValueError("Number of qubits must be at least 2 to create a Bell state.")
+            raise ValueError(
+                "Number of qubits must be at least 2 to create a Bell state."
+            )
 
         simulator = cls(num_qubits)
-        simulator.h(0)              # Apply Hadamard gate to the first qubit
-        simulator.cnot(0, 1)        # Apply CNOT gate with qubit 0 as control and qubit 1 as target
+        simulator.h(0)  # Apply Hadamard gate to the first qubit
+        simulator.cnot(
+            0, 1
+        )  # Apply CNOT gate with qubit 0 as control and qubit 1 as target
 
         # Remaining qubits (if any) are left in the |0⟩ state
         return simulator
 
     @classmethod
-    def setup_ghz_state(cls, num_qubits: int) -> 'QuantumSimulator':
+    def setup_ghz_state(cls, num_qubits: int) -> "QuantumSimulator":
         """
         Sets up a GHZ state with the specified number of qubits.
 
@@ -158,24 +173,27 @@ class QuantumSimulator:
             QuantumSimulator: An instance of QuantumSimulator with GHZ state prepared.
         """
         if num_qubits < 2:
-            raise ValueError("Number of qubits must be at least 2 to create a GHZ state.")
-        
+            raise ValueError(
+                "Number of qubits must be at least 2 to create a GHZ state."
+            )
+
         simulator = cls(num_qubits)
         simulator.h(0)  # Apply Hadamard gate to the first qubit
-        
+
         # Apply CNOT gates from the first qubit to all other qubits
         for target in range(1, num_qubits):
             simulator.cnot(0, target)
-        
+
         return simulator
-    
+
     @classmethod
-    def setup_phase_shift_example_state(cls) -> 'QuantumSimulator':
+    def setup_phase_shift_example_state(cls) -> "QuantumSimulator":
         simulator = cls(1)
         simulator.h(0)
         simulator.p(0, np.pi / 4)
         simulator.h(0)
         return simulator
+
 
 if __name__ == "__main__":
     # Create a Bell state with 2 qubits
--- nptRepr.py
+++ nptRepr.py
@@ -7,13 +7,14 @@ from itertools import product
 
 INDICES = string.ascii_lowercase
 
-def contract_tensors(A, B):
-    ...
 
-def outer_prod(v, u):
-    ...
+def contract_tensors(A, B): ...
 
-def init_state(n: int = 1, init_arr: List[float] = None)->np.array:
+
+def outer_prod(v, u): ...
+
+
+def init_state(n: int = 1, init_arr: List[float] = None) -> np.array:
     """
     creates a tensor for the initial state of a given number of qubits
 
@@ -25,11 +26,11 @@ def init_state(n: int = 1, init_arr: List[float] = None)->np.array:
     """
     # Start with a simple 2 x 2^(n-1) matrix that represents the initial state
     zero_qbit = np.array([1, 0])
-    
-    state_list = [zero_qbit]*n
+
+    state_list = [zero_qbit] * n
 
     if init_arr is not None:
-        state_list = [np.array([1-x, x]) for x in init_arr]
+        state_list = [np.array([1 - x, x]) for x in init_arr]
 
     state = state_list[0]
 
@@ -38,93 +39,121 @@ def init_state(n: int = 1, init_arr: List[float] = None)->np.array:
 
     return state
 
-#GATES
 
-def create_two_qbit_control_gate(gate:np.array) -> np.array:
+# GATES
+
+
+def create_two_qbit_control_gate(gate: np.array) -> np.array:
     C = np.array([[0, 0], [0, 1]])
     I = np.array([[1, 0], [0, 1]])
     SIG = gate
 
-    Control_1 = np.array([I-C, C], dtype=complex)
+    Control_1 = np.array([I - C, C], dtype=complex)
     Control_2 = np.array([I, SIG], dtype=complex)
 
     return np.einsum("ikj,lkm->ijlm", Control_1, Control_2)
 
-#TWO QUBIT GATES
 
-def CH()->np.array:
+# TWO QUBIT GATES
+
+
+def CH() -> np.array:
     return create_two_qbit_control_gate(H())
 
-def CNOT()->np.array:
+
+def CNOT() -> np.array:
     return create_two_qbit_control_gate(X())
+
 
 def CX() -> np.array:
     return create_two_qbit_control_gate(X())
 
+
 def CY() -> np.array:
     return create_two_qbit_control_gate(Y())
+
 
 def CZ() -> np.array:
     return create_two_qbit_control_gate(Z())
 
+
 def CS() -> np.array:
     return create_two_qbit_control_gate(S())
+
 
 def CSDG() -> np.array:
     return create_two_qbit_control_gate(SDG())
 
-#ONE QUBIT GATES
 
-def H()->np.array:
-    return 1/np.sqrt(2)*np.array([[1,1],[1,-1]],dtype=complex)
+def CT() -> np.array:
+    return create_two_qbit_control_gate(T())
 
-def X()->np.array:
-    return np.array([[0, 1], [1, 0]],dtype=complex)
 
-def Y()->np.array:
-    return np.array([[0, -1j], [1j, 0]],dtype=complex)
+def CP(theta: float) -> np.array:
+    return create_two_qbit_control_gate(P(theta))
+
+
+# ONE QUBIT GATES
+
+
+def H() -> np.array:
+    return 1 / np.sqrt(2) * np.array([[1, 1], [1, -1]], dtype=complex)
+
+
+def X() -> np.array:
+    return np.array([[0, 1], [1, 0]], dtype=complex)
+
+
+def Y() -> np.array:
+    return np.array([[0, -1j], [1j, 0]], dtype=complex)
+
 
 def Z() -> np.array:
-    return np.array([[1, 0], [0, -1]],dtype=complex)
+    return np.array([[1, 0], [0, -1]], dtype=complex)
+
 
 def S() -> np.array:
-    return np.array([[1,0],[0,1j]],dtype=complex)
+    return np.array([[1, 0], [0, 1j]], dtype=complex)
+
 
 def SDG() -> np.array:
-    return np.array([[1,0],[0,-1j]],dtype=complex)
+    return np.array([[1, 0], [0, -1j]], dtype=complex)
+
 
 def T() -> np.array:
-    return np.array([[1,0],[0,np.exp(1j*np.pi/4)]],dtype=complex)
-
-def P(theta:float) -> np.array:
-    return np.array([[1,0],[0,np.exp(1j * theta)]],dtype=complex)
-
-#APPLY GATES
+    return np.array([[1, 0], [0, np.exp(1j * np.pi / 4)]], dtype=complex)
 
 
-def apply_one_qubit_gate(state:np.array,gate:np.array,qbit:int)->np.array:
-    state=np.array(state) # perform deep copy
+def P(theta: float) -> np.array:
+    return np.array([[1, 0], [0, np.exp(1j * theta)]], dtype=complex)
+
+
+# APPLY GATES
+
+
+def apply_one_qubit_gate(state: np.array, gate: np.array, qbit: int) -> np.array:
+    state = np.array(state)  # perform deep copy
     # Number of dimensions of the state tensor
     num_dims = len(state.shape)
-    if(qbit>num_dims):
+    if qbit > num_dims:
         raise ValueError("qbit index bigger than dimension")
-    if(gate.shape!=(2,2)):
+    if gate.shape != (2, 2):
         raise ValueError("wrong gate shape")
-    
-    axis=num_dims-qbit-1 #convert to qiskit qbit numbering
 
-    #skip a and b chars
+    axis = num_dims - qbit - 1  # convert to qiskit qbit numbering
+
+    # skip a and b chars
     indices = [chr(99 + i) for i in range(num_dims)]
-    input_subs = ''.join(indices)
-    output_subs = ''.join(indices)
+    input_subs = "".join(indices)
+    output_subs = "".join(indices)
 
     # Replaces the k-th axis in input_subs with new index from gate_subs
     input_subs_list = list(input_subs)
     output_subs_list = list(output_subs)
-    input_subs_list[axis] = 'a'
-    output_subs_list[axis] = 'b'  
-    input_subs = ''.join(input_subs_list)
-    output_subs = ''.join(output_subs_list)
+    input_subs_list[axis] = "a"
+    output_subs_list[axis] = "b"
+    input_subs = "".join(input_subs_list)
+    output_subs = "".join(output_subs_list)
 
     # Construct the einsum string
     einsum_string = f'{input_subs},{"ab"}->{output_subs}'
@@ -133,60 +162,59 @@ def apply_one_qubit_gate(state:np.array,gate:np.array,qbit:int)->np.array:
     # Apply the gate using einsum
     return np.einsum(einsum_string, state, gate)
 
-def apply_two_qubit_gate(state:np.array,gate:np.array,qbit_one:int,qbit_two:int)->np.array:
+
+def apply_two_qubit_gate(
+    state: np.array, gate: np.array, qbit_one: int, qbit_two: int
+) -> np.array:
     num_dims = len(state.shape)
-    if(qbit_one>num_dims or qbit_two>num_dims):
+    if qbit_one > num_dims or qbit_two > num_dims:
         raise ValueError("qbit index bigger than dimension")
-    if(gate.shape!=(2,2,2,2)):
+    if gate.shape != (2, 2, 2, 2):
         raise ValueError("wrong gate shape")
-    axis1 = num_dims-qbit_one-1
-    axis2 = num_dims-qbit_two-1
-     # Build indices for einsum
+    axis1 = num_dims - qbit_one - 1
+    axis2 = num_dims - qbit_two - 1
+    # Build indices for einsum
     indices = [chr(101 + i) for i in range(num_dims)]
-    input_subs = ''.join(indices)
-    output_subs = ''.join(indices)
-    
+    input_subs = "".join(indices)
+    output_subs = "".join(indices)
+
     # Replace indices at axis1 and axis2
     input_subs_list = list(input_subs)
     output_subs_list = list(output_subs)
-    
-    input_subs_list[axis1] = 'a'
-    input_subs_list[axis2] = 'b'
-    output_subs_list[axis1] = 'c'
-    output_subs_list[axis2] = 'd'
-    
-    input_subs = ''.join(input_subs_list)
-    output_subs = ''.join(output_subs_list)
-    
-    einsum_str = f'{input_subs},acdb->{output_subs}'
+
+    input_subs_list[axis1] = "a"
+    input_subs_list[axis2] = "b"
+    output_subs_list[axis1] = "c"
+    output_subs_list[axis2] = "d"
+
+    input_subs = "".join(input_subs_list)
+    output_subs = "".join(output_subs_list)
+
+    einsum_str = f"{input_subs},acdb->{output_subs}"
     # Apply the gate using einsum
     return np.einsum(einsum_str, state, gate)
 
 
-
-
-
-
-#PLOTS
-def plot_nqbit_prob(MPS:np.array):
-    n=len(MPS.shape)
+# PLOTS
+def plot_nqbit_prob(MPS: np.array):
+    n = len(MPS.shape)
     prob_distr = np.abs(MPS).__pow__(2)
 
-    x_lbls = [''.join(state) for state in product('01', repeat=n)]
+    x_lbls = ["".join(state) for state in product("01", repeat=n)]
     probs = prob_distr.flatten()
 
     plt.bar(x_lbls, probs)
-    plt.xlabel('Quantum States')
-    plt.ylabel('Probability')
-    plt.title('Probability Distribution of nptRepr Quantum States')
+    plt.xlabel("Quantum States")
+    plt.ylabel("Probability")
+    plt.title("Probability Distribution of nptRepr Quantum States")
     plt.ylim(0, 1)
     plt.show()
 
 
 if __name__ == "__main__":
-    state=init_state(3)
+    state = init_state(3)
     print(state)
-    state=apply_one_qubit_gate(state,H(),0)
-    state=apply_one_qubit_gate(state,H(),2)
-    state=apply_two_qubit_gate(state,CY(),0,1)
+    state = apply_one_qubit_gate(state, H(), 0)
+    state = apply_one_qubit_gate(state, H(), 2)
+    state = apply_two_qubit_gate(state, CY(), 0, 1)
     print(state)
